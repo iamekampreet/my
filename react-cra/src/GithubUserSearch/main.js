924563f96522d77a53922056d107202abba5c1eb
Preet <iamekampreet@gmail.com>
2021-05-07 18:20:30 +0530

    Shifting back to linux, all data uploaded from windows

--- react-cra/src/GithubUserSearch/main.js
+++ react-cra/src/GithubUserSearch/main.js
@@ -1,32 +1,27 @@
 //This file will return the function that is going to get rendered.
-import React,{useState} from 'react';
+import React, { useState } from 'react';
 import './myCss.css';
 
-
-
-export default function Main(){
-    const [enteredValue,setEnteredValue]= useState('');
-    return(
+export default function Main() {
+    const [enteredValue, setEnteredValue] = useState('');
+    return (
         <div>
             <h1>Github User Search</h1>
-            <form >
-            <label htmlFor="searchUser">Enter Username</label>
-            <input 
-                type="text" 
-                id="searchUser" 
-                name="searchUser" 
-                onChange={(event)=>{
-                    setEnteredValue(event.target.value)
-                    // console.log(enteredValue);
-                    // console.log(event.target.value);
-                }}
-            />
-            <input type="submit" value="Submit" 
-                // onSubmit={event=>{
-                //     setEnteredValue(event.target.value);
-                //     console.log(enteredValue);
-                // }}
-            />
+            <form onSubmit={event => {
+                event.preventDefault();
+                console.log('Submit was pressed');
+            }}>
+                <label htmlFor="searchUser">Enter Username</label>
+                <input
+                    type="text"
+                    id="searchUser"
+                    name="searchUser"
+                    value={enteredValue}
+                    onChange={(event) => {
+                        setEnteredValue(event.target.value);
+                    }}
+                />
+                <button type="submit" >Submit</button>
             </form>
             <p>{enteredValue}</p>
         </div>
