38cd6eaf0c30a9611be9d1e517f8d9473852c736
itsrandom64 <iamekampreet@gmail.com>
2021-05-16 11:01:04 +0530

    Moving back to windows :P

--- practise/js/practiseScript.js
+++ practise/js/practiseScript.js
@@ -1,66 +1,16 @@
-'use strict';
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+const server = require("http").createServer((req, res) => {
+  res.writeHead(200, { "Content-type": "text/html" });
+  res.write(
+    '<form method="POST"><input type="text" name="placeholder"><button type="submit">Submit</button></form>'
+  );
+
+  req.on("data", (chunk) => {
+    console.log(chunk.toString());
+  });
+
+  res.end();
+});
+
+server.listen(5000, () => {
+  console.log("Server is running");
+});
